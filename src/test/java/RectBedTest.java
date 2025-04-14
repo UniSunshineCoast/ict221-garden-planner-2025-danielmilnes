841c8944fe307b2ef9d278b457600a8d049e1684
danielmilnes <danielmilnes@hotmail.com>
2025-04-14 12:00:41 +1000

    RectBed unit tests

--- src/test/java/RectBedTest.java
+++ src/test/java/RectBedTest.java
@@ -11,11 +11,39 @@ public class RectBedTest {
     void testGetArea() {
         RectBed rBed = new RectBed();
 
-        Assertions.assertEquals(1, rBed.getArea());
+        assertEquals(1, rBed.getArea());
 
         rBed.setWidth(2);
         rBed.setHeight(3);
         assertEquals(6, rBed.getArea());
     }
 
+    @Test
+    void testGettersAndSetters() {
+        RectBed rBed = new RectBed();
+        rBed.setHeight(7.0);
+        rBed.setWidth(5.5);
+        rBed.setTop(1.0);
+        rBed.setLeft(3.5);
+
+        assertEquals(7.0, rBed.getHeight());
+        assertEquals(5.5, rBed.getWidth());
+        assertEquals(1.0, rBed.getTop());
+        assertEquals(3.5, rBed.getLeft());
+    }
+
+    @Test
+    void testCalcs() {
+        RectBed rBed = new RectBed();
+        rBed.setHeight(7.0);
+        rBed.setWidth(5.5);
+
+        assertEquals(38.5, rBed.getArea());
+        assertEquals(25.0, rBed.getPerimeter());
+    }
+
+    @Test void testToString() {
+        RectBed rbed = new RectBed();
+        assertEquals("Rectangle 0.00 0.00 1.00 1.00", rbed.toString());
+    }
 }
