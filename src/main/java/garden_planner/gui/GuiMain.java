48a2c22cdbaa25f5d55064a755dc1a6ca66fc094
danielmilnes <danielmilnes@hotmail.com>
2025-04-21 12:12:21 +1000

    Added basic GUI

--- src/main/java/garden_planner/gui/GuiMain.java
+++ src/main/java/garden_planner/gui/GuiMain.java
@@ -8,7 +8,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -23,10 +25,28 @@ public class GuiMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         // Parent root = FXMLLoader.load(getClass().getResource("garden_gui.fxml"));
-        Button root = new Button("TODO: design garden planner GUI");
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+        // Set up garden
+        GardenPlanner planner = new GardenPlanner();
+        planner.createBasicDesign();
+
+        // Set up scene
+        primaryStage.setTitle("Garden");                    // Stage title
+        Pane pane = new HBox();                             // Create pane
+        pane.setStyle("-fx-background-color: #007700;");    // Set colour
+
+        // Loop through garden beds
+        for (RectBed bed: planner.getBeds()) {
+            // Add rectangle representing garden bed to pane
+            pane.getChildren().add(
+                    new Rectangle(bed.getWidth()*100, bed.getHeight()*100)
+            );
+            // Add padding
+            pane.getChildren().add(new Rectangle(50, 1)); // i got 99 problems but a syntax error aint one
+        }
+
+        primaryStage.setScene(new Scene(pane, 800, 600));   // Add pane to scene
+        primaryStage.show();    // Show scene
     }
 
 
