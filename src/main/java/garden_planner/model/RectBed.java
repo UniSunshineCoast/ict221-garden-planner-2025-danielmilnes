e2aba5a4abeffbca474f1774132c2a0139732e10
danielmilnes <danielmilnes@hotmail.com>
2025-04-22 11:13:40 +1000

    Changed tabs to 4-spaces like a civilised person

--- src/main/java/garden_planner/model/RectBed.java
+++ src/main/java/garden_planner/model/RectBed.java
@@ -6,61 +6,61 @@ package garden_planner.model;
  * @author Mark Utting
  */
 public class RectBed extends GardenBed {
-	private double width = 1.0;
-	private double height = 1.0;
+    private double width = 1.0;
+    private double height = 1.0;
 
-	public RectBed() {}
-	/**
-	 * Total width of this rectangle.
-	 *
-	 * @return width in metres.
-	 */
-	@Override
-	public double getWidth() {
-		return width;
-	}
+    public RectBed() {}
+    /**
+     * Total width of this rectangle.
+     *
+     * @return width in metres.
+     */
+    @Override
+    public double getWidth() {
+        return width;
+    }
 
-	public void setWidth(double width) {
-		this.width = width;
-	}
+    public void setWidth(double width) {
+        this.width = width;
+    }
 
-	/**
-	 * Total height of this rectangle.
-	 *
-	 * @return height in metres.
-	 */
-	@Override
-	public double getHeight() {
-		return height;
-	}
+    /**
+     * Total height of this rectangle.
+     *
+     * @return height in metres.
+     */
+    @Override
+    public double getHeight() {
+        return height;
+    }
 
-	public void setHeight(double height) {
-		this.height = height;
-	}
+    public void setHeight(double height) {
+        this.height = height;
+    }
 
-	/**
-	 * Get the area of this rectangle.
-	 *
-	 * @return the total internal area of the rectangle.
-	 */
-	@Override
-	public double getArea() {
-		return width * height;
-	}
+    /**
+     * Get the area of this rectangle.
+     *
+     * @return the total internal area of the rectangle.
+     */
+    @Override
+    public double getArea() {
+        return width * height;
+    }
 
-	/**
-	 * Get the perimeter of this rectangle.
-	 *
-	 * @return the total length of the edges of the rectangle.
-	 */
-	@Override
-	public double getPerimeter() {
-		return 2 * (width + height);
-	}
+    /**
+     * Get the perimeter of this rectangle.
+     *
+     * @return the total length of the edges of the rectangle.
+     */
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
 
-	@Override
-	public String toString() {
-		return String.format("Rectangle %.2f %.2f %.2f %.2f", left, top, width, height);
-	}
+    @Override
+    public String toString() {
+        return String.format("Rectangle %.2f %.2f %.2f %.2f", left, top, width, height);
+    }
 }
 
