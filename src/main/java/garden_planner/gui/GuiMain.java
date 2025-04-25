74a381311e38c2f7a3c77144764b2884d78e8969
danielmilnes <danielmilnes@hotmail.com>
2025-04-25 15:16:32 +1000

    Added bed info pane

--- src/main/java/garden_planner/gui/GuiMain.java
+++ src/main/java/garden_planner/gui/GuiMain.java
@@ -4,7 +4,10 @@ import garden_planner.model.GardenBed;
 import garden_planner.model.GardenPlanner;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -24,22 +27,35 @@ public class GuiMain extends Application {
         planner.createBasicDesign();
 
         // Set up scene
-        primaryStage.setTitle("Garden");                    // Stage title
-        Pane pane = new Pane();                             // Create pane
-        pane.setStyle("-fx-background-color: #007700;");    // Set colour
+        primaryStage.setTitle("Garden");                        // Stage title
+        Pane bedsPane = new Pane();                             // Create beds pane
+        bedsPane.setStyle("-fx-background-color: #007700;");    // Set colour
+        VBox textPane = new VBox();                             // Create text pane
+        BorderPane rootPane = new BorderPane();                 // Create root pane
+        rootPane.setCenter(bedsPane);                           // Put beds pane in root pane
+        rootPane.setBottom(textPane);                           // Put text pane in root pane
 
         // Loop through garden beds
+        int counter = 0;
         for (GardenBed bed: planner.getBeds()) {
-            // Add rectangle representing garden bed to pane
+            counter++;
+            // Add rectangle representing garden bed to bedsPane
             Rectangle rect = new Rectangle();       // Create rectangle
             rect.setWidth(bed.getWidth()*100);      // Set width
             rect.setHeight(bed.getHeight()*100);    // Set height
             rect.setX(bed.getLeft()*100);           // Set X
             rect.setY(bed.getTop()*100);            // Set Y
-            pane.getChildren().add(rect);           // Add to pane
+            bedsPane.getChildren().add(rect);       // Add to bedsPane
+
+            // Add bed info
+            Text bedInfoText = new Text();            // Create text node
+            bedInfoText.setText(
+                    "Bed " + counter + "\tWidth: " + bed.getWidth() + "m\tHeight: " + bed.getHeight() +
+                    "m\tArea: " + bed.getArea() + "m^2\tPerimeter: " + bed.getPerimeter() + "m");
+            textPane.getChildren().add(bedInfoText);
         }
 
-        primaryStage.setScene(new Scene(pane, 800, 600));   // Add pane to scene
+        primaryStage.setScene(new Scene(rootPane, 800, 600));   // Add root pane to scene
         primaryStage.show();    // Show scene
     }
 
